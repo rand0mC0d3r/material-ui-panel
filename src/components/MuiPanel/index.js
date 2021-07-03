c1f48cbd90cfaa12576d2d6fb3660bc956794031
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2021-07-03 20:55:41 +0200

    updated packages

--- src/components/MuiPanel/index.js
+++ src/components/MuiPanel/index.js
@@ -1,3 +1,4 @@
+import Paper from '@material-ui/core/Paper';
 import React from 'react';
 
 const MuiPanel = ({
@@ -5,13 +6,13 @@ const MuiPanel = ({
   isExternal = false,
   title
 }) => (
-  <div style={isExternal ? {
+  <Paper style={isExternal ? {
     position: "absolute",
     bottom: "0px"
   } : {}}>
     <div>{icon} {title}</div>
     test panel method
-  </div>
+  </Paper>
 )
 
 export default MuiPanel;
