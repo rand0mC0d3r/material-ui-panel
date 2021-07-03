8e4b2b993f526e9fdd9f2f15b0301fcbfa50dd62
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2021-07-03 19:02:51 +0200

    first runs

--- src/components/MuiPanel/index.js
+++ src/components/MuiPanel/index.js
@@ -1,36 +1,13 @@
-import Icon from '@material-ui/core/Icon';
-import PropTypes from 'prop-types';
 import React from 'react';
-
-const sizesMap = {
-  'small': { size: 17, extraSize: 10 },
-  'medium': { size: 25, extraSize: 13 },
-  'large': { size: 25, extraSize: 15 },
-}
 
 const MuiPanel = ({
   icon,
-  extraIcon,
-  size = 'small',
-  color = 'inherit',
-  position = 'bottom-end',
-  disabled,
+  title
 }) => (
-  <div style={{
-    position: 'relative',
-    cursor: 'default'
-  }}>
+  <div>
+    <div>{icon} {title}</div>
     test panel
   </div>
 )
-
-MuiPanel.propTypes = {
-  icon: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
-  extraIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  color: PropTypes.string,
-  position: PropTypes.oneOf(['top-start', 'top-end', 'bottom-start', 'bottom-end']),
-  disabled: PropTypes.bool,
-};
 
 export default MuiPanel;
