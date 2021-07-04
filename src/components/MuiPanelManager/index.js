bcdadb2479ec21e9b10554068548537fc9a57585
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2021-07-04 18:29:34 +0200

    updated UI

--- src/components/MuiPanelManager/index.js
+++ src/components/MuiPanelManager/index.js
@@ -7,9 +7,12 @@ import React, { cloneElement, useState } from 'react';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    height: "100%",
+    position: "absolute",
+    width: "100%",
     display: "grid",
-    "grid-template-columns": "1fr 1fr 1fr",
-    "grid-template-rows": "1fr 1fr 1fr",
+    "grid-template-columns": "1fr 3fr 1fr",
+    "grid-template-rows": "1fr 3fr 1fr",
     "gap": "0px 0px",
     "grid-auto-flow": "row",
     "grid-template-areas":`
