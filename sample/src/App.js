d901005ef54a7c8e0f1a002fc81268ac063b58ec
rand0mC0d3r <practiseo@gmail.com>
2021-07-17 22:46:33 +0200

    updated UI

--- sample/src/App.js
+++ sample/src/App.js
@@ -2,6 +2,7 @@ import CameraIcon from '@material-ui/icons/Camera';
 import CastIcon from '@material-ui/icons/Cast';
 import ChromeReaderModeIcon from '@material-ui/icons/ChromeReaderMode';
 import CropSquareIcon from '@material-ui/icons/CropSquare';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import Skeleton from '@material-ui/lab/Skeleton';
 import './App.css';
@@ -16,12 +17,16 @@ import NotificationPanel from './parts/NotificationPanel';
 function App() {
   return <DataContextProvider>
     <MuiPanelManager>
+      <MuiDivider icon={<GitHubIcon />} tooltip="Only icon separator" />
+      <MuiDivider tooltip="Default separator" />
+      <MuiDivider showIcon={false} shortText={"MENU"} tooltip="Default separator" />
+
       {/* <MuiDivider shortText="////" tooltip="No icon separator" showIcon={false} /> */}
       <MuiPanel title="First Panel" icon={<CameraIcon />}>
         {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget purus vitae ipsum tempus aliquam imperdiet quis erat. Pellentesque tellus massa, tincidunt sit amet rutrum eget, finibus sit amet mauris. Aliquam erat volutpat. Fusce placerat rutrum placerat. Curabitur aliquam bibendum tristique. In porta velit ac augue auctor tempus. Sed eget nunc a ligula ultrices euismod ut ac mi. Pellentesque pellentesque auctor diam. Interdum et malesuada fames ac ante ipsum primis in faucibus.`}
       </MuiPanel>
 
-      <MuiDivider tooltip="Default separator" />
+
       <MuiPanel title="SubFunny Panel Skeletons" icon={<CastIcon />}>
         <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
         <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
@@ -29,7 +34,7 @@ function App() {
         <Skeleton animation="wave" height={10} width="80%" />
       </MuiPanel>
 
-      <MuiDivider icon={<NotificationsIcon />} tooltip="Only icon separator" />
+
       <MuiPanel title="SubFunny Panel Text" subTitle="No icon big guy..." icon={<CastIcon />}>sub 2b</MuiPanel>
       <MuiPanel title="Rain Panel" icon={<ChromeReaderModeIcon />} initialSide="left">4</MuiPanel>
 
