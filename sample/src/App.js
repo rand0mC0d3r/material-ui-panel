03771e345a629a4be07e81d8e504d1ca527c01db
rand0mC0d3r <practiseo@gmail.com>
2021-07-17 23:00:42 +0200

    updated UI

--- sample/src/App.js
+++ sample/src/App.js
@@ -21,11 +21,9 @@ function App() {
       <MuiDivider tooltip="Default separator" />
       <MuiDivider showIcon={false} shortText={"MENU"} tooltip="Default separator" />
 
-      {/* <MuiDivider shortText="////" tooltip="No icon separator" showIcon={false} /> */}
       <MuiPanel title="First Panel" icon={<CameraIcon />}>
         {`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget purus vitae ipsum tempus aliquam imperdiet quis erat. Pellentesque tellus massa, tincidunt sit amet rutrum eget, finibus sit amet mauris. Aliquam erat volutpat. Fusce placerat rutrum placerat. Curabitur aliquam bibendum tristique. In porta velit ac augue auctor tempus. Sed eget nunc a ligula ultrices euismod ut ac mi. Pellentesque pellentesque auctor diam. Interdum et malesuada fames ac ante ipsum primis in faucibus.`}
       </MuiPanel>
-
 
       <MuiPanel title="SubFunny Panel Skeletons" icon={<CastIcon />}>
         <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
@@ -34,37 +32,15 @@ function App() {
         <Skeleton animation="wave" height={10} width="80%" />
       </MuiPanel>
 
+      <MuiPanel title="SubFunny Panel Text" subTitle="No icon big guy..." icon={<CastIcon />}>
+        <Skeleton variant="rect" width={'100%'} height={600} />
+      </MuiPanel>
 
-      <MuiPanel title="SubFunny Panel Text" subTitle="No icon big guy..." icon={<CastIcon />}>sub 2b</MuiPanel>
-      <MuiPanel title="Rain Panel" icon={<ChromeReaderModeIcon />} initialSide="left">4</MuiPanel>
-
-      {/* <MuiDivider shortText="////" tooltip="No icon separator" showIcon={false} />
-
-      <MuiDivider shortText="ABCD" tooltip="No icon separator" showIcon={false} />
-      <MuiDivider shortText="ABCD" tooltip="Icon and text" icon={<NotificationsIcon />} />
-      <MuiDivider shortText="ABCDEFGH" tooltip="Icon and long long text" icon={<NotificationsIcon />} /> */}
-
-
-
-      {/* <MuiDivider title="Notifications" />
-      <MuiPanel title="Notifications Panel" icon={<NotificationsIcon />} initialSide="left">
+      <MuiPanel title="Rain Panel" icon={<ChromeReaderModeIcon />} initialSide="left">
         <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
         <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
         <Skeleton animation="wave" height={10} width="80%" />
       </MuiPanel>
-      <NotificationPanel />
-
-
-      <MuiDivider title="Headers and Icons" />
-      <MuiPanel title="Square Iconless" initialSide="left" subTitle="No icon big guy...">
-        <Skeleton variant="rect" width={'100%'} height={600} />
-      </MuiPanel>
-      <MuiPanel title="Square Icon Disabled" icon={<CropSquareIcon />} iconInHeader={false} initialSide="left" subTitle="No icon big guy...">
-        <Skeleton variant="rect" width={'100%'} height={600} />
-      </MuiPanel>
-      <MuiPanel title="Square Sample" icon={<CropSquareIcon />} initialSide="left" subTitle="A big guy...">
-        <Skeleton variant="rect" width={'100%'} height={600} />
-      </MuiPanel> */}
 
       <div>
         <iframe
