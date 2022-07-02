00816b63f5d10521389787aa6a0137cca35da592
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-07-02 20:21:05 +0200

    updated UI

--- sample/src/parts/ConfigStatus.js
+++ sample/src/parts/ConfigStatus.js
@@ -4,10 +4,14 @@ import { Skeleton } from '@material-ui/lab';
 import MupStatus from '../components/MupStatus';
 import MupStatusChild from '../components/MupStatusChild';
 
-export default () =>  <MupStatus
-    asMenu={<div style={{ width: '350px', padding: '16px', margin: '16px'}}>
+export default () => {
+
+  console.log("a")
+
+  return <MupStatus
+    asMenu={<div style={{ width: '350px', padding: '16px', margin: '16px' }}>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed euismod nisi. Duis nec commodo augue. Curabitur fringilla efficitur lacus, scelerisque convallis ex consequat at. Maecenas gravida volutpat porta. Integer viverra et sapien eget dictum. Sed fringilla pharetra bibendum. Cras gravida diam in orci hendrerit commodo. Integer eget lobortis leo, a pellentesque tortor. Fusce vel libero pulvinar, pellentesque neque et, aliquam mauris. Suspendisse at vulputate sapien, eget sodales elit. Integer eleifend blandit dolor, ac ultricies nibh consectetur ut.
-      <div style={{marginTop: '16px', display: "flex", gap: "8px", flexDirection: "column"}}>
+      <div style={{ marginTop: '16px', display: "flex", gap: "8px", flexDirection: "column" }}>
         <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
         <Skeleton animation="wave" height={10} />
         <Skeleton variant="rect" width={'100%'} height={200} />
@@ -21,3 +25,4 @@ export default () =>  <MupStatus
   >
     <MupStatusChild icon={<SettingsIcon />} />
   </MupStatus>
+}
