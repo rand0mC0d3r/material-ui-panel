96b9349851d4b1713efaf7d156b98d879f7c42b6
Bogdan Dumitrescu <bogdan.dumitrescu@ada.com>
2022-07-11 00:54:26 +0200

    updated UI

--- sample/src/parts/AliveStatus.js
+++ sample/src/parts/AliveStatus.js
@@ -1,32 +1,33 @@
-import WifiIcon from '@material-ui/icons/Wifi'
-import WifiOffIcon from '@material-ui/icons/WifiOff'
+import SignalWifi1BarIcon from '@material-ui/icons/SignalWifi1Bar'
+import SignalWifi4BarIcon from '@material-ui/icons/SignalWifi4Bar'
 import { useEffect, useState } from 'react'
 import MupStatus from '../components/MupStatus'
 import MupStatusChild from '../components/MupStatusChild'
 
 const breakpoint = 60
+const max = 300
 
 export default () => {
-  const [speed, setSpeed] = useState(75)
-  const [highlight, setHighlight] = useState(false)
+  const [speed, setSpeed] = useState(175)
+  const [highlight, setHighlight] = useState('default')
 
   useEffect(() => {
     const interval = setInterval(() => {
       const signChosen = Math.ceil(Math.random() * 250) > 210 ? '-1' : '1'
-      const number = speed + signChosen * Math.ceil(Math.random() * 50)
-      setHighlight(number < breakpoint)
+      const number = speed + signChosen * Math.ceil(Math.random() * 150)
+      setHighlight(number < breakpoint ? 'secondary' : number < max ? 'default' : 'primary')
       setSpeed(() => number)
     }, 750)
 
     return () => clearInterval(interval)
   }, [])
 
-  return <MupStatus {...{ highlight: highlight ? 'secondary' : 'default' }}
+  return <MupStatus {...{ highlight }}
     secondary
     style={{ minWidth: '90px' }}
     id='statusAlive'
     tooltip={`Server connection bandwidth ${speed} KB/s`}
   >
-    <MupStatusChild icon={highlight ? <WifiOffIcon /> :  <WifiIcon />} text={`${speed} KB/s`} />
+    <MupStatusChild icon={highlight === 'secondary' ? <SignalWifi1BarIcon /> :  <SignalWifi4BarIcon />} text={`${speed} KB/s`} />
   </MupStatus>
 }
